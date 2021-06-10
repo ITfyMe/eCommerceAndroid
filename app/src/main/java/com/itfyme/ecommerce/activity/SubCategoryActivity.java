67f2f6fd7f8004d40564902fe95ae25ed9abbee9
THUNGA <karthik@kaholabs.com>
2021-06-10 18:42:42 +0530

    created activity

---
+++ app/src/main/java/com/itfyme/ecommerce/activity/SubCategoryActivity.java
@@ -0,0 +1,16 @@
+package com.itfyme.ecommerce.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.itfyme.ecommerce.R;
+
+public class SubCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_category);
+    }
+}
