08f131d8d9e0eb1ecdc7d00bd61cae321262aef4
aditya31031998 <adityam31031998@gmail.com>
2021-06-11 02:26:56 -0700

    adding nav

--- app/src/main/java/com/itfyme/ecommerce/activity/LandingPageActivity.java
+++ app/src/main/java/com/itfyme/ecommerce/activity/LandingPageActivity.java
@@ -1,17 +1,12 @@
 package com.itfyme.ecommerce.activity;
-
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.ui.AppBarConfiguration;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.google.android.material.navigation.NavigationView;
 import com.itfyme.ecommerce.R;
 
