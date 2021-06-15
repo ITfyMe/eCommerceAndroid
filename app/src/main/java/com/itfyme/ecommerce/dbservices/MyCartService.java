6d1cacc6d6130ce927806c1e07b9dcec1fd13921
aditya31031998 <adityam31031998@gmail.com>
2021-06-15 07:49:42 -0700

    cart serices

--- app/src/main/java/com/itfyme/ecommerce/dbservices/MyCartService.java
+++ app/src/main/java/com/itfyme/ecommerce/dbservices/MyCartService.java
@@ -1,4 +1,107 @@
 package com.itfyme.ecommerce.dbservices;
 
+import android.content.Context;
+
+import com.itfyme.ecommerce.interfaces.ResponseHandler;
+import com.itfyme.ecommerce.network.VolleyNetworkManager;
+
+import java.util.HashMap;
+
 public class MyCartService {
+    Context mContext;
+    private String getListByPageURL ="MyCart/get-list-object-page";
+    private String getListURL       ="MyCart/get-list";
+    private String getURL           ="MyCart/get";
+    //    private String addURL           ="MyCart/add";
+    private String updateURL        ="MyCart/update";
+    private String deleteURL        ="MyCart/delete";
+
+
+    public MyCartService(Context context) {
+        mContext = context;
+
+    }
+
+
+
+    public void getCartList(HashMap<String, String> params, ResponseHandler responseHandler) {
+        new VolleyNetworkManager(mContext).getRequest(getListURL, params, new ResponseHandler() {
+            @Override
+            public void onSuccess(Object data) {
+                try {
+                    responseHandler.onSuccess(data);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFail(Object data) {
+                responseHandler.onFail("Failed to load data");
+
+            }
+
+            @Override
+            public void onNoData(Object data) {
+                responseHandler.onFail("No data available");
+            }
+        });
+
+
+    }
+
+
+    public void updateCart(HashMap<String, String> params, ResponseHandler responseHandler) {
+        new VolleyNetworkManager(mContext).postRequest(updateURL, params, new ResponseHandler() {
+            @Override
+            public void onSuccess(Object data) {
+                try {
+                    responseHandler.onSuccess(data);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFail(Object data) {
+                responseHandler.onFail("Failed to update data");
+
+            }
+
+            @Override
+            public void onNoData(Object data) {
+
+            }
+        });
+
+
+    }
+    public void deleteCart(String id, ResponseHandler responseHandler) {
+        HashMap<String, String> params=new HashMap<>();
+        params.put("Categoryid",id);
+        new VolleyNetworkManager(mContext).postRequest(deleteURL, params, new ResponseHandler() {
+            @Override
+            public void onSuccess(Object data) {
+                try {
+                    responseHandler.onSuccess(data);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFail(Object data) {
+                responseHandler.onFail("Failed to delete data");
+
+            }
+
+            @Override
+            public void onNoData(Object data) {
+
+            }
+        });
+
+
+    }
+
 }
